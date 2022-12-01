671896cb3b76ca5de134d0d99940ce84320c9c82
Eberson <eberson.santos1@gmail.com>
2022-12-01 20:46:43 -0300

    atualizado código com alterações solicitadas pelo codacy

--- src/algoritmos/MergeSort.java
+++ src/algoritmos/MergeSort.java
@@ -45,7 +45,9 @@ public class MergeSort {
 	private static void merge(
       int[] a, int[] l, int[] r, int left, int right) {
      
-        int i = 0, j = 0, k = 0;
+        int i = 0;
+        int j = 0;
+        int  k = 0;
         while (i < left && j < right) {
             if (l[i] <= r[j]) {
                 a[k++] = l[i++];
