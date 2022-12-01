286305651f1364d2304016f486b7b5496a5e628e
rcaa <rodrigo.andrade@ufape.edu.br>
2022-12-01 13:14:57 -0300

    adicionando metodo para leitura de arquivo

--- src/exemplos/Main.java
+++ src/exemplos/Main.java
@@ -1,10 +1,15 @@
 package exemplos;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		f(10);
+		readFile("");
 	}
 
 	public static long f(int n) {
@@ -15,4 +20,24 @@ public class Main {
             return f(n - 1) + f(n - 2);
         }
     }
+	
+	public static String readFile(String fileName){ 
+	    BufferedReader br; 
+	    try { 
+	        br = new BufferedReader(new FileReader(fileName)); 
+	        final StringBuilder sb = new StringBuilder(); 
+	        String line = br.readLine(); 
+	 
+	        while (line != null) { 
+	            sb.append(line); 
+	            sb.append(System.lineSeparator()); 
+	            line = br.readLine(); 
+	        } 
+	        br.close(); 
+	        return sb.toString(); 
+	    } catch (IOException e) { 
+	        e.printStackTrace(); 
+	    } 
+	    return ""; 
+	}
 }
